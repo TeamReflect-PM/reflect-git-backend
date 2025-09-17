902aa100909190314ede2b38beb9faaa5c76b3fd
Teamreflect <teamreflect.dev@gmail.com>
2025-09-17 14:58:10 +0000

    postgres production config and setup

--- config.py
+++ config.py
@@ -1,10 +1,17 @@
+from dotenv import load_dotenv
+import os
+
+# Load .env automatically
+load_dotenv()
+
+
 # config.py
 PROJECT_ID = "reflect-466215"
 REGION = "us-central1"
 EMBEDDING_MODEL = "text-embedding-005"
 #Postgres SQL connection
-PG_HOST = "127.0.0.1"       # your DB host/IP
-PG_DB = "reflect_db"           # your database name
-PG_USER = "postgres"       # your DB user
-PG_PASSWORD = "Reflect@123"  # your DB password
-PG_PORT = 5432                 # default PostgreSQL port
+PG_HOST = os.getenv("PG_HOST", "127.0.0.1")
+PG_DB = os.getenv("PG_DB", "reflect_db")
+PG_USER = os.getenv("PG_USER", "postgres")
+PG_PASSWORD = os.getenv("PG_PASSWORD", "Reflect@123")
+PG_PORT = int(os.getenv("PG_PORT", 5432))
